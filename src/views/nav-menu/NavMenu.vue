85880d311a8adb468834c154935110ead3350f8c
pjqdyd <1909025079@qq.com>
2019-11-08 19:06:44 +0800

    update

--- src/views/nav-menu/NavMenu.vue
+++ src/views/nav-menu/NavMenu.vue
@@ -5,7 +5,7 @@
       :default-active="pageIndex"
       :active-text-color="$store.state.theme"
       text-color="#304156"
-      class="el-menu-demo nav-menu"
+      class="nav-menu"
       style="box-shadow: 0  1px 10px rgba(0, 0, 150, .2);"
       mode="horizontal"
       @select="handleSelect"
@@ -58,25 +58,32 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+$nav-height: 50px;
+$nav-bgColor:  rgba($color: #ffffff, $alpha: 0.2);
+
 .nav-menu {
   position: fixed;
   top: 0;
   width: 100%;
-  height: 50px;
-  background: rgba($color: #ffffff, $alpha: 0.2);
+  height: $nav-height;
+  background: $nav-bgColor;
   display: flex;
   justify-content: flex-end;
   align-items: center;
   .el-menu-item {
-    height: 50px;
-    line-height: 50px;
+    height: $nav-height;
+    line-height: $nav-height;
   }
   .el-item-box {
-    margin: 15px;
+    margin: 15px; 
     color: #304156;
   }
 }
- .el-menu--horizontal {
-    border-bottom: 0 !important;
-  }
+.el-menu--horizontal {
+  border-bottom: 0 !important;
+}
+
+.el-menu-item:not(.is-disabled):focus, .el-menu-item:not(.is-disabled):hover, .el-submenu__title:hover {
+    background-color: $nav-bgColor;
+}
 </style>
