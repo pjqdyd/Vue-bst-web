90db3954a9098b82cd6fac4bc3214f00bd198b58
pjqdyd <1909025079@qq.com>
2020-02-03 17:10:45 +0800

    添加软件部card

--- src/views/page-2/PageTwo.vue
+++ src/views/page-2/PageTwo.vue
@@ -2,7 +2,17 @@
 <template>
   <div>
     <div id="page-two" class="page-two" :style="{height: $pageHeight+'px' }">
-     
+      <!-- 软件部标题 -->
+      <div class="title-box">软件部</div>
+
+      <!-- 软件部的信息卡片 -->
+      <div class="row-card-box">
+        <el-row id="el-row" type="flex" justify="space-around">
+          <el-col span="5" v-for="index in 5" :key="index">
+            <software-card></software-card>
+          </el-col>
+        </el-row>
+      </div>
     </div>
     <!-- 分割线 -->
     <el-divider content-position="center">2</el-divider>
@@ -10,21 +20,41 @@
 </template>
 
 <script>
+import SoftwareCard from "./component/software-card.vue"; //软件部卡片
 
 export default {
   components: {
+    SoftwareCard
   },
   data() {
-    return {
-    };
+    return {};
   },
   mounted() {
+    //当在移动端时, 设置卡片box的宽度, 使超出的卡片可横向滚动
+    if (navigator.userAgent.match(/(phone|iPhone|ios|Android|Mobile|IEMobile)/i)) {
+      var elRow = document.getElementById("el-row");
+      elRow.style.width = document.documentElement.clientWidth * 5 + "px";
+    }
   },
   methods: {}
 };
 </script>
 
 <style lang='scss' scoped>
-  .page-two{
+.page-two {
+  z-index: 0;
+  .title-box {
+    width: 100%;
+    height: 80px;
+    color: #000;
+    text-align: center;
   }
+
+  .row-card-box {
+    margin: auto;
+    width: 95%;
+    height: auto;
+    overflow-x: auto;
+  }
+}
 </style>
---
+++ src/views/page-2/component/software-card.vue
@@ -0,0 +1,36 @@
+<!-- 软件卡片 -->
+<template>
+  <div class="card-box">
+    <el-card :body-style="{ padding: '5px' }" shadow="hover">
+      <img
+        src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+        class="image"
+      />
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  mounted() {
+  },
+  methods: {}
+};
+</script>
+
+<style lang='scss' scoped>
+.card-box {
+  margin: 0 2% 0 2%;
+  .el-card {
+      border-radius: 10px;
+  }
+  .image {
+    width: 100%;
+    display: block;
+  }
+}
+</style>
