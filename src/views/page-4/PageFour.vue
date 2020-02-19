5fe62668648e8187c58ff2aafcfa919e70f1444e
pjqdyd <1909025079@qq.com>
2020-02-19 22:14:57 +0800

    添加mac-console组件

--- src/views/page-4/PageFour.vue
+++ src/views/page-4/PageFour.vue
@@ -3,15 +3,23 @@
   <div>
      <div id="page-four" class="page-four"  :style="{height: $pageHeight+'px' }">
 
+       <!-- Mac风格控制窗 -->
+       <div class="mac-box">
+          <console-mac></console-mac>
+       </div>
+
      </div>
-     <!-- 分割线 -->
-    <el-divider content-position="center">4</el-divider>
-  </div>
+  </!-->
 </template>
 
 <script>
+
+import ConsoleMac from "@/components/console-mac/index.vue"; //Mac风格控制窗组件
+
 export default {
-  components: {},
+  components: {
+    ConsoleMac
+  },
   data() {
     return {
     };
@@ -23,4 +31,14 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+.page-four{
+  .mac-box{
+    width: 500px;
+    height: 500px;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+}
 </style>
