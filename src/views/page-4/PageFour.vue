39aa6985d94b7ea8ce446934ac22915af145b3ba
pjqdyd <1909025079@qq.com>
2020-04-02 22:29:26 +0800

    update

--- src/views/page-4/PageFour.vue
+++ src/views/page-4/PageFour.vue
@@ -3,11 +3,15 @@
   <div>
      <div id="page-four" class="page-four"  :style="{height: $pageHeight+'px' }">
 
-       <!-- Mac风格控制窗 -->
-       <div class="mac-box">
-          <console-mac></console-mac>
-       </div>
+      <!-- Mac风格控制窗 -->
+      <div class="mac-box">
+        <console-mac></console-mac>
+      </div>
 
+      <!-- 底部信息盒子 -->
+      <div class="bottom-info-box" :style="{color: $store.state.theme}">
+        <p class="bottom-info-text">© 2019 - 2020 蓝色技术工作室 江西科技学院</p>
+      </div> 
      </div>
 
   </div>
@@ -15,7 +19,7 @@
 
 <script>
 
-import ConsoleMac from "@/components/console-mac/index.vue"; //Mac风格控制窗组件
+import ConsoleMac from "./component/console-mac.vue"; //Mac风格控制窗组件
 
 export default {
   components: {
@@ -33,6 +37,9 @@ export default {
 
 <style lang='scss' scoped>
 .page-four{
+  display: flex;
+  flex-direction: column;
+  justify-items: self-end;
   .mac-box{
     width: 500px;
     height: 500px;
@@ -40,6 +47,15 @@ export default {
     display: flex;
     flex-direction: column;
     justify-content: center;
+  }
+  .bottom-info-box{
+    width: 100%;
+    height: 60px;
+    opacity: .9;
+    background-color: #282c34;
+    .bottom-info-text{
+      font-size: 16px;
+    }
   }
 }
 </style>
